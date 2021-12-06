3de1e083c1960c4746072c2c18c0f4505cc9b9f5
hanjoohyung <ghd167@naver.com>
2021-12-06 15:36:21 +0900

    api:emaillist

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,25 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import EmailList from './EmailList';
 
 import './assets/scss/App.scss';
-import data from './assets/json/data.json';
+// import data from './assets/json/data.json';
 
 
 export default function() {
+    const [emails, setEmails] = useState([]);
     const [keyword, setkeyword] = useState('');
 
     const notifyKeywordChanged = (keyword) => {
         console.log(keyword);
         setkeyword(keyword);
-    }
+    };
+
+    useEffect(async () => {
+        try {
+            const response = await fetch('/api', {
+                                        method: 'get',
+                                        headers: {
+                                            'Content-Type': 'application/json',
+                                            'Accept': 'application'
+                                        },
+                                        body: null        
+            });
+
+            if(!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const jsonResult = await response.json();
+
+            if(jsonResult.result !== 'success') {
+                throw new Error(`${jsonResult.result} ${jsonResult.message}`);
+            }
+
+            setEmails(jsonResult.data);
+
+        } catch(err) {
+        console.error(err);
+        }
+    }, []);
 
     return (
         <div className={'App'}>
           <RegisterForm />
           <SearchBar keyword={keyword} callback={notifyKeywordChanged} />
-          <EmailList keyword={keyword} emails={data} />
+          <EmailList keyword={keyword} emails={emails} />
         </div>
     )
 }
